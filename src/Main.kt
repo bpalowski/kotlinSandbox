6c97a30a4d5168acb1df25f8b9d2b89a4a2ba504
Bran <brianpalowski00@protonmail.com>
2023-12-19 17:09:55 -0600

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,55 @@
+import java.util.*
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+//    //this is a comment
+//    val name = "my cats name is \"fluffy\""
+////    println(name)
+//    var slashes = "there is 2 types of :\\/"
+//    var homePage = "hhtp://yes.com"
+////    println(homePage)
+//
+//
+//
+//    var first= "(\\ (\\"
+//    var second = "(-.-)"
+//    var third= "o_(\")(\")"
+//    println(first)
+//    println(second)
+//    println(third)
+
+//    val name = " this is a test"
+//    println(name.lowercase())
+//    println(name.trim())
+//    println(name.get(4))
+//    println(name.substring(3,5))
+//
+//
+//    // expressions
+//    println("hello ${1+1}")
+
+// USER INPUT with numbers as well
+//    println("type ...")
+//    val userInput= readLine();
+//    val numberInput = readLine()?:""
+//    println("you wrote: $userInput: ${numberInput.toInt()}");
+
+
+// Random
+//   println( Random.nextInt(10,30) )
+//    println( Random.nextDouble(100.1) )
+
+//Bday
+
+//
+    println("Whats your Bday year?")
+    val currentYear= readLine()?:""
+    println("What year is it?")
+    val birthYear= readLine()?:""
+    println("you are ${birthYear.toInt()-currentYear.toInt() }")
+
+
+
+
+}
+
